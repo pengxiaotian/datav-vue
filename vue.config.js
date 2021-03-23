5337021e1f45c890b8ba640585d311d1a8d0368d
Allen <673923162@qq.com>
2021-03-23 18:54:46 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 
 module.exports = {
-  publicPath: '/datav-vue/',
+  publicPath: '/',
   devServer: {
     host: '0.0.0.0',
     port: 9090,
