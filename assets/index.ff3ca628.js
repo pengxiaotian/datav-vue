63d4b8509a8da27770a9ba96d9c51c290436266a
pengxiaotian <673923162@qq.com>
2021-07-11 15:07:27 +0000

    website deploy

---
+++ assets/index.ff3ca628.js
@@ -0,0 +1,1 @@
+import{d as s,o as a,c as t,a as e,r as n}from"./index.d9e1f3b2.js";var o=s({name:"GDropList"});const r={class:"drop-list-wp"},i={class:"item-container"};o.render=function(s,o,d,l,c,m){return a(),t("div",r,[e("div",i,[n(s.$slots,"default")])])},o.install=s=>{s.component(o.name,o)};export default o;
