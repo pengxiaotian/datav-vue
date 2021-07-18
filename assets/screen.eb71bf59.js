ef190eda3501df3539486fe0809d45547c793733
pengxiaotian <673923162@qq.com>
2021-07-18 03:54:15 +0000

    website deploy

--- assets/screen.eb71bf59.js
+++
@@ -1,1 +0,0 @@
-import{v as e}from"./index.7a1b8f24.js";async function t(t){await e.get("/screen",{params:{screenId:t}});try{const e=localStorage.getItem("DataV-Preview");if(e)return JSON.parse(e)}catch(a){localStorage.removeItem("DataV-Preview")}return null}function a(t){return localStorage.setItem("DataV-Preview",JSON.stringify(t)),e.post("/screen",t)}export{t as g,a as s};
---
+++ assets/screen.ee179b59.js
@@ -0,0 +1,1 @@
+import{v as e}from"./index.5753b56b.js";async function t(t){await e.get("/screen",{params:{screenId:t}});try{const e=localStorage.getItem("DataV-Preview");if(e)return JSON.parse(e)}catch(r){localStorage.removeItem("DataV-Preview")}return null}function r(t){return localStorage.setItem("DataV-Preview",JSON.stringify(t)),e.post("/screen",t)}export{t as g,r as s};
