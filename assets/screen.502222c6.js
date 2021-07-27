22a1ee5a0b114a10229c591f740ee2e2043793e6
pengxiaotian <673923162@qq.com>
2021-07-27 06:22:44 +0000

    website deploy

--- assets/screen.502222c6.js
+++
@@ -1,1 +0,0 @@
-import{v as e}from"./index.3fbdeb38.js";async function t(t){await e.get("/screen",{params:{screenId:t}});try{const e=localStorage.getItem("DataV-Preview");if(e)return JSON.parse(e)}catch(r){localStorage.removeItem("DataV-Preview")}return null}function r(t){return localStorage.setItem("DataV-Preview",JSON.stringify(t)),e.post("/screen",t)}export{t as g,r as s};
