49f1bbe3ed31ef057d2486e06bb8036ee86683c2
pengxiaotian <673923162@qq.com>
2021-07-27 14:18:10 +0800

    chore: 修改示例模板

--- src/data/mock.ts
+++ src/data/mock.ts
@@ -1,6 +1,7 @@
 import { EditorModule } from '@/store/modules/editor'
 import { createComponent } from '@/components/datav'
 import { getRandomInt } from '@/utils/util'
+import { BlueprintModule } from '@/store/modules/blueprint'
 
 export const useMock = () => {
   const { pageConfig } = EditorModule
@@ -13,5 +14,11 @@ export const useMock = () => {
     com.attr.x = getRandomInt(pageConfig.width - com.attr.w)
     com.attr.y = getRandomInt(pageConfig.height - com.attr.h)
     EditorModule.addCom(com)
+
+    if (com.apis.source) {
+      com.loadData().then(() => {
+        BlueprintModule.datavComponents[com.id].$DATAV_requestData()
+      })
+    }
   })
 }
