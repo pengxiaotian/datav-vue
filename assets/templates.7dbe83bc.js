192b917beb7ca1fc149464d1ee20e77ac89ef657
pengxiaotian <673923162@qq.com>
2021-07-31 17:44:45 +0000

    website deploy

--- assets/templates.7dbe83bc.js
+++
@@ -1,1 +0,0 @@
-import{x as t}from"./index.3afedbee.js";async function e(){return t.get("/templates/tpl-list.json")}async function n(e){return t.get(`/templates/tpl-${e}.json`)}export{n as a,e as g};
