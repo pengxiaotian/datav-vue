b9c2148af99df5c6f788551647ebe395d17f2c68
pengxiaotian <673923162@qq.com>
2021-12-29 06:37:19 +0000

    website deploy

--- assets/data.3e250bba.js
+++
@@ -1,1 +0,0 @@
-import{aq as o}from"./vendor.5472ebc5.js";async function n(a,t){return o.get(`/data/${t}.json?comId=${a}`)}export{n as g};
--- assets/templates.40f6a538.js
+++
@@ -1,1 +0,0 @@
-import{aq as t}from"./vendor.5472ebc5.js";async function a(){return t.get("/templates/tpl-list.json")}async function n(e){return t.get(`/templates/tpl-${e}.json`)}export{n as a,a as g};
