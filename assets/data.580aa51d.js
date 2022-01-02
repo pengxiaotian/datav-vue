7d83738dd403df1060752f1e3e188f5c1e7e7c5b
pengxiaotian <673923162@qq.com>
2022-01-02 17:17:15 +0000

    website deploy

---
+++ assets/data.580aa51d.js
@@ -0,0 +1,1 @@
+import{ap as o}from"./vendor.0227b33d.js";async function r(a,t){return o.get(`/data/${t}.json?comId=${a}`)}export{r as g};
---
+++ assets/templates.7b09f335.js
@@ -0,0 +1,1 @@
+import{ap as t}from"./vendor.0227b33d.js";async function a(){return t.get("/templates/tpl-list.json")}async function n(e){return t.get(`/templates/tpl-${e}.json`)}export{n as a,a as g};
