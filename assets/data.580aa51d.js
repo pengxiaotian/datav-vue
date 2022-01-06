d3b6bfd96bdf3470e7979b7bda3ba2793958d657
pengxiaotian <673923162@qq.com>
2022-01-06 09:11:57 +0000

    website deploy

--- assets/data.580aa51d.js
+++
@@ -1,1 +0,0 @@
-import{ap as o}from"./vendor.0227b33d.js";async function r(a,t){return o.get(`/data/${t}.json?comId=${a}`)}export{r as g};
--- assets/screen.e6ae4b3f.js
+++
@@ -1,1 +0,0 @@
-import{e as a}from"./App.4d672324.js";const t="DataV-Preview";async function s(e){await a.get("/screen",{params:{screenId:e}});try{const r=localStorage.getItem(t);if(r)return JSON.parse(r)}catch{localStorage.removeItem(t)}return null}function o(e){return localStorage.setItem(t,JSON.stringify(e)),a.post("/screen",e)}export{s as g,o as s};
