3f4e4d6321c1a85e10733adb66779e70533a1baa
pengxiaotian <673923162@qq.com>
2022-12-10 17:54:11 +0000

    Deploying to gh-pages from @ pengxiaotian/datav-vue@4c1acbc203a7143b620fe834f332413491fde019 🚀

--- assets/templates.1d620fcb.js
+++
@@ -1,1 +0,0 @@
-import{ar as t}from"./vendor.891943b7.js";async function a(){return t.get("/templates/tpl-list.json")}async function n(e){return t.get(`/templates/tpl-${e}.json`)}export{a,n as g};
---
+++ assets/templates.2d00e6e8.js
@@ -0,0 +1,1 @@
+import{K as t}from"./App.5d51c89e.js";async function a(){return t.get("/templates/tpl-list.json")}async function n(e){return t.get(`/templates/tpl-${e}.json`)}export{a,n as g};
