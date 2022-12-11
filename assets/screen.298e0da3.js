3fb8e8e177f41c0045fa100a0390e7ed8a46deb3
pengxiaotian <673923162@qq.com>
2022-12-11 09:45:33 +0000

    Deploying to gh-pages from @ pengxiaotian/datav-vue@9059daf59b02d535577903acbc99bbfeaf2f4ac8 🚀

---
+++ assets/screen.298e0da3.js
@@ -0,0 +1,1 @@
+import{J as a}from"./App.49df06d9.js";const t="DataV-Preview";async function o(e){await a.get("/screen",{params:{screenId:e}});try{const r=localStorage.getItem(t);if(r)return JSON.parse(r)}catch{localStorage.removeItem(t)}return null}function s(e){return localStorage.setItem(t,JSON.stringify(e)),a.post("/screen",e)}export{o as g,s};
--- assets/screen.ce294341.js
+++
@@ -1,1 +0,0 @@
-import{J as a}from"./App.5d51c89e.js";const t="DataV-Preview";async function o(e){await a.get("/screen",{params:{screenId:e}});try{const r=localStorage.getItem(t);if(r)return JSON.parse(r)}catch{localStorage.removeItem(t)}return null}function s(e){return localStorage.setItem(t,JSON.stringify(e)),a.post("/screen",e)}export{o as g,s};
